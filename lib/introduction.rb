25d5bc64649b0f13526537b96788f05dfbf9687f
Cody Shaben <cshaben87@gmail.com>
2019-07-07 16:00:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,4 +4,7 @@ end
 
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-  
+
+def introduction_with_language_optional(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}." 
+  if 
